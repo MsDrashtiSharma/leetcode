1e415425af05ae9fe76819fbdb3c30e201a90592
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-01-24 22:40:49 +0530

    Time: 24 ms (61.37%), Space: 13.5 MB (70.82%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-     /*   bool dfs(int i,vector<int>adj[],bool v[],bool dv[])
+        bool dfs(int i,vector<int>adj[],int v[],int dv[])
         {
-             v[i]=true;
-                dv[i]=true;
+             v[i]=1;
+                dv[i]=1;
                 for(auto it:adj[i]){
                         if(!v[it]){
                                 if(dfs(it,adj,v,dv))
@@ -13,17 +13,17 @@ public:
                                            return true;
                                    }
                         }
-                                  dv[i]=false;
+                                  dv[i]=0;
                                    return false;
         }
 bool canFinish(int numcourses, vector<vector<int>>& prerequisites) {
-      bool v[numcourses],dv[numcourses];
-        memset(v,false,sizeof v);
-        memset(dv,false,sizeof v);
+      int v[numcourses],dv[numcourses];
+        memset(v,0,sizeof v);
+        memset(dv,0,sizeof v);
     vector<int>adj[numcourses];
         for(int i=0;i<prerequisites.size();i++)
         {
-                adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+               adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
             for(int i=0;i<numcourses;i++)
             {
@@ -37,45 +37,6 @@ bool canFinish(int numcourses, vector<vector<int>>& prerequisites) {
                     
             }
             return true;
-    }*/
-        bool canFinish(int numcourses, vector<vector<int>>& prerequisites) {
-    vector<vector<int>>adj(numcourses,vector<int>());
-            for(vector<int>&pre:prerequisites)
-                    adj[pre[0]].push_back(pre[1]);
-            vector<int>v(numcourses,0);
-            vector<int>dv(numcourses,0);
-            for(int i=0;i<numcourses;i++)
-            {
-                    if(!v[i])
-                    {
-                            if(dfs(i,adj,v,dv))
-                            {
-                                    return false;
-                            }
-                    }
-                    
-            }
-            return true;
     }
-        bool dfs(int i,vector<vector<int>>&adj,vector<int>&v,vector<int>&dv)
-        {
-             v[i]=1;
-                dv[i]=1;
-                for(auto it:adj[i])
-                {
-                        if(!v[it])
-                        {
-                                if(dfs(it,adj,v,dv))
-                                   return true;
-                        }
-                                   else if(dv[it])
-                                   {
-                                           return true;
-                                   }
-                        
-                }
-                                  dv[i]=0;
-                                   return false;
-        }
         
 };
