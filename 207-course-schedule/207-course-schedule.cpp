d05b54331657669f8d3ff369c151416132cebda3
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-01-24 22:36:30 +0530

    Time: 20 ms (82.48%), Space: 13.3 MB (80.27%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    bool checkCycle(int start, bool checkPrequisite[], bool visitDFS[], vector<int> graph[]){
-             checkPrequisite[start] = true;
-             visitDFS[start] = true;
-                
-             for(auto i : graph[start]){
-                   if(!checkPrequisite[i]){   
-                          if(checkCycle(i, checkPrequisite, visitDFS, graph))
-                                 return true;
-                      }
-                    else if(visitDFS[i])
-                           return true;
-             }
-            
-        visitDFS[start] = false;
-        return false;
-   }
-    
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-      
-        bool checkPrequisite[numCourses] , visitDFS[numCourses];
-        
-        memset (checkPrequisite, false, sizeof checkPrequisite);
-        memset (visitDFS, false, sizeof visitDFS);
-        
-        vector<int> graph[numCourses];
-        
-        // creating a graph using prerequisites
-        for(int i = 0; i < prerequisites.size(); i++){
-            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        bool dfs(int i,vector<int>adj[],bool v[],bool dv[])
+        {
+             v[i]=true;
+                dv[i]=true;
+                for(auto it:adj[i]){
+                        if(!v[it]){
+                                if(dfs(it,adj,v,dv))
+                                   return true;
+                        }    else if(dv[it])
+                                   {
+                                           return true;
+                                   }
+                        }
+                                  dv[i]=false;
+                                   return false;
         }
-        
-         for(int i = 0; i < numCourses; i++){
-            if(!checkPrequisite[i]){
-                if(checkCycle(i, checkPrequisite, visitDFS, graph))     // if cycle exists we can't finish all courses
-                    return false;
+bool canFinish(int numcourses, vector<vector<int>>& prerequisites) {
+      bool v[numcourses],dv[numcourses];
+        memset(v,false,sizeof v);
+        memset(dv,false,sizeof v);
+    vector<int>adj[numcourses];
+        for(int i=0;i<prerequisites.size();i++)
+        {
+                adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+            for(int i=0;i<numcourses;i++)
+            {
+                    if(!v[i])
+                    {
+                            if(dfs(i,adj,v,dv))
+                           
+                                    return false;
+                            
+                    }
+                    
             }
-        }
-        return true;
-        
+            return true;
     }
+        
 };
