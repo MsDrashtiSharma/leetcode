a7115dc70884fbdddc1589a7b409397875e4de2f
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-01-30 11:53:12 +0530

    Time: 77 ms (9.60%), Space: 19.6 MB (11.84%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -3,23 +3,23 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
            queue<vector<int>>q;
             vector<vector<int>>res;
-         int startindex=0;
+           int startindex=0;
            int target=graph.size()-1;
             vector<int>v;
             v.push_back(startindex);
             q.push(v);
             while(!q.empty())
-        {  int size=q.size();
-         while(size-->0)
-         {vector<int>temp=q.front();
+           {int size=q.size();
+           while(size-->0)
+           {vector<int>temp=q.front();
                     q.pop();
                     if(temp.back()==target)
                             res.push_back(temp);
                     else
                     {
-                        for(auto v:graph[temp.back()])
+                        for(auto i:graph[temp.back()])
                         { vector<int>ans=temp;
-                            ans.push_back(v);
+                            ans.push_back(i);
                             q.push(ans);
                         }
                            
