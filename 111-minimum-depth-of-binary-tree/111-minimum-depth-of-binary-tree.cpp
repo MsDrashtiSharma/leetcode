8846cff4b4f41b43018a19dd951073764a70b12d
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-02-05 12:27:17 +0530

    Time: 432 ms (25.46%), Space: 144.7 MB (73.01%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-   // special case : empty tree
+  /* // special case : empty tree
         if (root == nullptr) return 0;
         // DFS
         int left = minDepth(root->left);
@@ -21,6 +21,33 @@ public:
         //  1) left/right tree has/both empty, empty one will return 0, "left + right + 1"
         //  2) left/right tree both not empty, both return positive depth, "std::min() + 1" 
         return (left == 0 || right == 0) ? left + right + 1 : min(left, right)+1;
+        */
+        
+        if(!root) return 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        int level=0;
+            while(!q.empty())
+            
+            {
+                int s=q.size();
+                level++;
+               for(int i=0;i<s;i++)
+                {
+                    TreeNode *parent=q.front();
+                    q.pop();
+                    if(parent->left)
+                        q.push(parent->left);
+                    if(parent->right)
+                        q.push(parent->right);
+                    if(!parent->left&&!parent->right)
+                    
+                        return level;
+                    
+                }
+            }
+        return -1;
+        
         
     }
 };
