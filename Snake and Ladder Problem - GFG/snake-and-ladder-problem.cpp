b7ba7f0f4f2e1fa876f868e1d38bbdc34f383cdc
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-02-21 11:20:58 +0530

    Added solution - LeetHub

---
+++ Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minThrow(int N, int arr[]){
+        // code here
+       
+     int moves[35];
+     bool vis[35];
+     memset(moves,-1,sizeof(moves));
+     memset(vis,false,sizeof(vis));
+     for(int i=0;i<2*N;i+=2)
+     {
+        moves[arr[i]]=arr[i+1]; 
+     }
+      queue<pair<int,int>>q;
+        pair<int,int>p;
+     q.push({1,0});
+     vis[1]=true;
+     while(!q.empty())
+     {
+         p=q.front();
+         int cell=p.first;
+         int turn=p.second;
+         if(cell==30)
+         break;
+         
+         q.pop();
+        for(int i=cell+1;i<=cell+6 && i<=30;i++)
+             {
+                 pair<int,int>pp;
+                 if(!vis[i])
+                 {
+                     pp.second=turn+1;
+                     vis[i]=true;
+                     if(moves[i]!=-1)
+                     {
+                         pp.first=moves[i];
+                     }
+                     else
+                     {
+                         pp.first=i;
+                     }
+                     q.push(pp);
+                 }
+             }
+     }
+     return p.second;
+     
+     
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[2*N];
+        for(int i = 0;i < 2*N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.minThrow(N, arr)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
