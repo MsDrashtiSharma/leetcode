b13069a46e9d1e72e164b32722dc845a5362691b
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-04-18 06:29:11 +0530

    Time: 36 ms (74.74%), Space: 6.5 MB (84.25%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -20,7 +20,7 @@ public:
         return f(n-1,m-1,s,t,dp);
     
 }*/
-     int numDistinct(string s, string t) {
+    /* int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
         vector<vector<double>>dp(n+1,vector<double>(m+1,-1));
@@ -44,5 +44,28 @@ public:
             
          }
           return (int)dp[n][m];
+     }*/
+     int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+       vector<double>prev(m+1,0),curr(m+1,0);
+         
+        prev[0]=curr[0]=1;
+         for(int i=1;i<=n;i++)
+         {
+             for(int j=1;j<=m;j++)
+             {
+                 if(s[i-1]==t[j-1])
+                 {
+                      curr[j]=prev[j-1]+prev[j];
+                 }
+                 else
+                     curr[j]=prev[j];
+             }
+             prev=curr;
+             
+            
+         }
+          return (int)prev[m];
      }
 };
