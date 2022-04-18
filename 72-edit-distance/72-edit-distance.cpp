e43ca89436cb3888771ee6fded3eb1877def80e9
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-04-18 07:48:24 +0530

    Time: 8 ms (88.38%), Space: 6.4 MB (97.83%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -13,6 +13,7 @@ public:
             return dp[i][j]=1+min(f(i-1,j,dp,s1,s2), min(f(i,j-1,dp,s1,s2),f(i-1,j-1,dp,s1,s2)));
                                     //insert            // delete               //replace
     }
+    }    
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int  m=s2.size();
@@ -20,7 +21,7 @@ public:
         return f(n-1,m-1,dp,s1,s2);
     }*/
     
-    int minDistance(string s1, string s2) {
+  /*  int minDistance(string s1, string s2) {
         int n=s1.size();
         int  m=s2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
@@ -38,6 +39,26 @@ public:
                     dp[i][j]=1+min(dp[i-1][j], min(dp[i][j-1],dp[i-1][j-1]));
             }
         }
-        return dp[n][m];
-    }    
+        return dp[n][m];*/
+     int minDistance(string s1, string s2) {
+        int n=s1.size();
+        int  m=s2.size();
+       vector<int>prev(m+1,0),curr(m+1,0);
+         for(int j=0;j<=m;j++)
+            prev[j]=j;
+       
+       
+        for(int i=1;i<=n;i++)
+        {  curr[0]=i;
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    curr[j]=prev[j-1];
+                else
+                    curr[j]=1+min(prev[j], min(curr[j-1],prev[j-1]));
+            }
+            prev=curr;
+        }
+        return prev[m];
+     }
 };
