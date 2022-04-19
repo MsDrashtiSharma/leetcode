73fd996255cb7c0fc0a3bad08e2d98d0b01e9b70
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-04-19 07:06:29 +0530

    Time: 756 ms (16.71%), Space: 207.1 MB (15.20%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(int ind,int buy,vector<int>& prices,int n, vector<vector<vector<int>>>&dp,int cap)
+   /* int f(int ind,int buy,vector<int>& prices,int n, vector<vector<vector<int>>>&dp,int cap)
     {
         if(ind==n||cap==0)
             return 0;
@@ -23,5 +23,50 @@ public:
        int n=prices.size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
         return f(0,1,prices,n,dp,2);
-    }
+    }*/
+    
+    
+       int maxProfit(vector<int>& prices) {
+       int n=prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+      /*  for(int ind=0;ind<n-1;ind++)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                dp[ind][buy][0]=0;
+            }
+        }
+     
+            for(int buy=0;buy<=1;buy++)
+            { for(int cap=0;cap<=2;cap++)
+               {
+                dp[n][buy][cap]=0;
+               }
+            }
+           */
+              for(int ind=n-1;ind>=0;ind--)
+           {
+               for(int buy=0;buy<=1;buy++)
+                   
+               {   int profit=0;
+                   for(int cap=1;cap<=2;cap++)
+                   {
+                                  if(buy==1)
+                        {
+                             profit=max(-prices[ind]+dp[ind+1][0][cap],0+dp[ind+1][1][cap]);
+                        }
+                       else
+                       {
+                               profit=max(prices[ind]+dp[ind+1][1][cap-1],0+dp[ind+1][0][cap]);            
+                       }
+                     dp[ind][buy][cap]=profit;  
+                   }
+               }
+           }
+         return dp[0][1][2];
+     
+           
+       }
+    
+    
 };
