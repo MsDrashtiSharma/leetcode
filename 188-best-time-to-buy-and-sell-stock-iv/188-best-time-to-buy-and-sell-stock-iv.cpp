770b3e342a01ea321001892f61392f6214f657ab
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-04-19 07:52:38 +0530

    Time: 805 ms (8.01%), Space: 407.2 MB (5.14%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
-    /*     int n=prices.size();
+    /*int maxProfit(int k, vector<int>& prices) {
+       int n=prices.size();
         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
        for(int ind=0;ind<=n;ind++)
         {
@@ -40,7 +40,12 @@ public:
          return dp[0][1][k];
      
          */
-         int n=prices.size();
+        
+        
+        
+        
+        //space optimaxtion in dp[2][k]
+      /*  int n=prices.size();
         vector<vector<int>>after(2,vector<int>(k+1,0));
         vector<vector<int>>curr(2,vector<int>(k+1,0));
       
@@ -67,9 +72,62 @@ public:
            }
          return after[1][k];
      
-      
-       }
+      }
            
-        
+   */    
+    int f(int ind,int transno,vector<int>prices,int n,int k,vector<vector<int>>&dp)
+    {if(ind==n||transno==2*k)
+        return 0;
+     if(dp[ind][transno]!=-1)
+         return dp[ind][transno];
+     if(transno%2==0)
+     {
+        // return dp[ind][transno]=max(-prices[ind]+dp[ind+1][transno+1],0+dp[ind+1][tansno]);
+          return dp[ind][transno]=max(-prices[ind]+f(ind+1,transno+1,prices,n,k,dp),0+f(ind+1,transno,prices,n,k,dp));
+     }
+        else
+        {
+            return dp[ind][transno]=max(prices[ind]+f(ind+1,transno+1,prices,n,k,dp),0+f(ind+1,transno,prices,n,k,dp));
+
+         //return dp[ind][transno]=max(prices[ind]+dp[ind+1][transno+1],0+dp[ind+1][tansno]);
+        }
+     //return dp[0][0];
+    }
+    int maxProfit(int k, vector<int>& prices) 
+    {  int n=prices.size();
+       vector<vector<int>>dp(n,vector<int>(2*k,-1));
+        return f(0,0,prices,n,k,dp);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 };
