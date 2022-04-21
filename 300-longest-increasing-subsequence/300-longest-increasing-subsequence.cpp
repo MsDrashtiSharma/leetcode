ff411c6784718567bf30128065cb5032f5275ce7
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-04-21 10:52:48 +0530

    Time: 572 ms (10.83%), Space: 10.7 MB (17.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //tc=O(n^2)  sc=O(n^2)
+    //tc=O(n^2)  sc=O(n^2)+O(n)-->auxillary space
   /*  int f(vector<int>& nums,int ind,int prev_ind,vector<vector<int>>&dp,int n)
     {
         if(ind==n)return 0;
@@ -21,7 +21,9 @@ public:
         return f(nums,0,-1,dp,n);
     }*/
     
-    int lengthOfLIS(vector<int>& nums) {
+    
+    //tc=o(n^2)  sc=o(n^2)
+  /*  int lengthOfLIS(vector<int>& nums) {
        int n=nums.size();
        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         for(int ind=n-1;ind>=0;ind--)
@@ -38,5 +40,30 @@ public:
             
         }
         return dp[0][-1+1];
+        */
+    
+      //tc=o(n^2)   sc=o(n*2)
+    int lengthOfLIS(vector<int>& nums) {
+       int n=nums.size();
+       vector<int>after(n+1,0),curr(n+1,0);
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--)
+            {
+                int len =0+after[prev_ind+1];
+                if(prev_ind==-1||nums[ind]>nums[prev_ind])
+                {
+                    len=max(len,1+after[ind+1]);
+                }
+                curr[prev_ind+1]=len;
+            }
+            after=curr;
+            
+        }
+        return after[-1+1];
     }
+      
+    
+    
+    
 };
