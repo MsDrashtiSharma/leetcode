25039a2b87b844dc8d41ab428249583f35700a85
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2022-04-21 14:13:28 +0530

    Time: 522 ms (14.01%), Space: 10.7 MB (12.68%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -66,8 +66,8 @@ public:
     
     
     
-//USING TABULATION ALGORITHM OR TABULATION ANOTHER APPROACH
-   int lengthOfLIS(vector<int>& nums) {
+//USING TABULATION ALGORITHM OR TABULATION ANOTHER APPROACH   tc=O(n*n)   sc=O(n)
+  /* int lengthOfLIS(vector<int>& nums) {
        int n=nums.size();
        vector<int>dp(n,1);
        int maxi=1;
@@ -84,10 +84,42 @@ public:
        }
        
        return maxi;
-   }
+   }*/
        
        
-       
+       int lengthOfLIS(vector<int>& nums) {
+       int n=nums.size();
+           int maxi=1;
+           int lastindex=0;
+       vector<int>dp(n,1),hash(n);
+           for(int i=0;i<n;i++)
+           {
+               hash[i]=i;
+               for(int prev=0;prev<i;prev++)
+               {
+                   if(nums[prev]<nums[i]&&1+dp[prev]>dp[i])
+                   {
+                       dp[i]=1+dp[prev];
+                       hash[i]=prev;
+                   }
+               }
+                    if(dp[i]>maxi)
+                    {
+                        maxi=dp[i];
+                        lastindex=i;
+                    }
+           }
+           vector<int>temp;
+           temp.push_back(nums[lastindex]);
+           while(hash[lastindex]!=lastindex){
+              lastindex=hash[lastindex];
+           temp.push_back(nums[lastindex]);
+           }
+           
+           reverse(temp.begin(),temp.end());
+           for(auto it:temp)cout<<it<<" "<<endl;
+           return maxi;
+       }
        
        
        
