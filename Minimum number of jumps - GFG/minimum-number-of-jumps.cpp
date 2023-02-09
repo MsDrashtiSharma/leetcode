191da1274faa49e08a3ba1e642c20e0d87cd6122
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2023-02-09 14:18:16 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
@@ -10,32 +10,56 @@ class Solution{
   public:
     int minJumps(int arr[], int n){
         // Your code here
+    //   if(n<=1)
+    //     {
+    //         return 0;
+    //     }
+    //     if(arr[0]==0)
+    //     return -1;
+    //     int maxreach=arr[0];
+    //     int steps=arr[0];
+    //     int jump=1;
+    //     int i=1;
+    //     for(i=1;i<n;i++)
+    //     {
+    //         if(i==n-1)
+    //         return jump;
+    //         maxreach=max(maxreach,arr[i]+i);
+    //         steps--;
+    //         if(steps==0)
+    //         {jump++;
+    //           if(i>=maxreach)
+    //           {
+    //             return -1;
+    //             }
+    //         steps=maxreach-i;
+    //         }
+    //     }
+    //     return -1;
         if(n<=1)
-        {
-            return 0;
-        }
-        if(arr[0]==0)
-        return -1;
+        return 0;
+        if(arr[0]==0)return -1;
         int maxreach=arr[0];
         int steps=arr[0];
-        int jump=1;
-        int i=1;
-        for(i=1;i<n;i++)
+        int jumps=1;
+        for(int i=1;i<n;i++)
         {
             if(i==n-1)
-            return jump;
+            return jumps;
             maxreach=max(maxreach,arr[i]+i);
             steps--;
             if(steps==0)
-            {jump++;
-               if(i>=maxreach)
-               {
-                return -1;
-                }
-            steps=maxreach-i;
+            {
+                jumps++;
+                if(i>=maxreach)
+                {return -1;}
+                steps=maxreach-i;
+                
             }
+            
         }
         return -1;
+        
     }
 };
 
