0ddf0b4ef84adfe677a7ef18036f586a5a72ab89
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2023-03-04 21:20:38 +0530

    Added solution - LeetHub

---
+++ Minimum Swaps for Bracket Balancing - GFG/minimum-swaps-for-bracket-balancing.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int minimumNumberOfSwaps(string s){
+        vector<int>v;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        if(s[i]=='[')v.push_back(i);
+        
+        int ind=0;
+        int ans=0;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='[')
+            {
+                count++;
+                ind++;
+            }
+            else
+            {
+                count--;
+                if(count<0)
+                {
+                    ans+=v[ind]-i;
+                    swap(s[i],s[v[ind]]);
+                    count=1;
+                    ind++;
+                }
+            }
+        }
+        return ans;
+    }    
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.minimumNumberOfSwaps(S) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
