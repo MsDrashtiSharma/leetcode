6d9d015a9084b184c89451415714fb5b715edea2
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2023-05-23 14:36:50 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -22,45 +22,28 @@ class Solution
 	    for(int i=0;i<V;i++)
 	    {
 	        if(indegree[i]==0)
-	          q.push(i);
+	        {
+	            q.push(i);
+	        }
 	    }
 	    vector<int>topo;
 	    while(!q.empty())
 	    {
-	       int node=q.front();
-	       q.pop();
-	       topo.push_back(node);
-	       for(auto it:adj[node])
-	       {
-	           indegree[it]--;
-	           if(indegree[it]==0)
-	           q.push(it);
-	       }
-	      
+	        int node=q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        for(auto it:adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	            {
+	                q.push(it);
+	            }
+	        }
 	    }
 	    return topo;
 	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //{ Driver Code Starts.
 
