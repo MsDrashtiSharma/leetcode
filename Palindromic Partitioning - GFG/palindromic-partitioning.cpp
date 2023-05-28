6cf8490243793d19226f9b5e59fde1615a8616a8
DRASHTI SHARMA <93121225+MsDrashtiSharma@users.noreply.github.com>
2023-05-28 14:59:02 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class Solution{
 public:
-int ispalindrome(int i,int j,string s)
+bool ispalindrome(int i,int j,string s)
 {
     while(i<j)
     {
@@ -19,44 +19,28 @@ int ispalindrome(int i,int j,string s)
     }
     return true;
 }
-int f(int i,int n, string s,vector<int>&dp)
-{
-    if(i==n)return 0;
-    int mincost=INT_MAX;
-    if(dp[i]!=-1)return dp[i];
-    for(int j=i;j<n;j++)
+    int f(int i,int n,string s,vector<int>&dp)
     {
-        if(ispalindrome(i,j,s))
+        if(i==n)return 0;
+        int mincost=INT_MAX;
+        if(dp[i]!=-1)return dp[i];
+        for(int j=i;j<=n;j++)
         {
-           int cost=1+f(j+1,n,s,dp);
-            mincost=min(cost,mincost);
+            if(ispalindrome(i,j,s))
+            {
+                int cost=1+f(j+1,n,s,dp);
+                mincost=min(mincost,cost);
+            }
         }
+        return dp[i]=mincost;
     }
-    return dp[i]=mincost;
-}
     int palindromicPartition(string str)
     {
         // code here
-        int n=str.size();
-        vector<int>dp(n+1,0);
         
-        for(int i=n-1;i>=0;i--)
-        {
-        //     if(i==n)continue;
-            int mincost=INT_MAX;
-            
-            for(int j=i;j<n;j++)
-            {
-                if(ispalindrome(i,j,str))
-                {
-                   int cost=1+dp[j+1];
-                    mincost=min(cost,mincost);
-                }
-            }
-            dp[i]=mincost;
-            
-        }
-        return dp[0]-1;
+        int n=str.size();
+        vector<int>dp(n+1,-1);
+        return f(0,n,str,dp)-1;
         
     }
 };
